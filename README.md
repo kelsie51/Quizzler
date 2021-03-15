fc70499d27b9dcb787765217e8e5a0a007f3b359
Kelsie <kelsie51@hotmail.com>
2021-03-15 01:23:08 -0600

    Added question module

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Quizzler
-A short timed code quiz.
+# Password-Generator
+
+</h1> Password Generator </h1>
+</p> Password generator uses 8-128 characters for security enhancement.
+</P> Allows users to input Special Characters, Alphabet inputs (Uppercase and lowercase), and numerals to generate a random password.
--- script.js
+++ script.js
@@ -1,9 +1,33 @@
+var questions = ["Are you a new student?", "Look a airplane?"] ;
+var question1Options = ["Yes", "No"];
+var question2Options = ["I saw a airplane", "That's a pretty cool airplane"];
+var rightWrong = ["Great job!", "Incorrect"];
+
 
 document.querySelector("#btnQuizStart").addEventListener("click", function () {
 
     question1.classList.remove('hide');
-    startquiz.classList.add('hide');   
+    startquiz.classList.add('hide');  
+    
+    document.getElementsByName('question1TextName')[0].placeholder = questions[0];
+    document.getElementById('btnQuestion1Option1').innerText = question1Options[0];
+    document.getElementById('btnQuestion1Option2').innerText = question1Options[1];
 });
+//TODO: research querySelector
+document.querySelector("#btnQuestion1Option1").addEventListener("click", function () {
+    document.getElementsByName('question1TextName')[0].placeholder = rightWrong[0]; //Placeholder puts "example" text in an object like "mm/dd/yyyy" in a birthdate textbox
+    question1Options.classList.add('hide'); 
+});
+document.querySelector("#btnQuestion1Option2").addEventListener("click", function () {
+    document.getElementsByName('question1TextName')[0].placeholder = rightWrong[1]; //Placeholder puts "example" text in an object like "mm/dd/yyyy" in a birthdate textbox
+    
+});
+
+
+function showHide(show, hide){
+    show.classList.remove('hide');
+    hide.classList.add('hide'); 
+}
 
 
 
